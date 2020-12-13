b583a666c52968ddac7781b1ed5ef5e251598060
DongEokSeo2 <de.seo@newploy.net>
2020-12-13 22:05:55 +0900

    swagger

---
+++ swagger/src/test/java/com/peterica/swagger/SwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peterica.swagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
